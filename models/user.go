4085e185daadc38ed7caf73fd91a639998317969
w0ofix <dev-wolfix@proton.me>
2026-07-17 02:55:09 +0200

    feat: added user model

---
+++ models/user.go
@@ -0,0 +1,17 @@
+package models
+
+type Ips struct {
+	RegisteredIp string   `json:"registered_ip"`
+	LoginIps     []string `json:"login_ips"`
+}
+
+type User struct {
+	BaseModel
+	Email    string `gorm:"unique;not null" json:"email"`
+	Username string `gorm:"unique;not null" json:"username"`
+	Password string `gorm:"not null" json:"-"`
+	Role     string `gorm:"default:user" json:"role"`
+	Bio      string `json:"bio"`
+	Avatar   string `json:"avatar"`
+	Ips      Ips    `gorm:"serializer:json" json:"ips"`
+}
