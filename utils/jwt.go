e725033f52cacb38d520e63b346a0eeb0fdb40e5
w0ofix <dev-wolfix@proton.me>
2026-07-17 05:07:01 +0200

    feat: Added JWT gen and parse

---
+++ utils/jwt.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"os"
+	"time"
+	"strconv"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	UserID   string `json:"user_id"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID, email, username string) (string, error) {
+	hours, err := strconv.Atoi(os.Getenv("JWT_EXPIRES_HOURS"))
+	if err != nil {
+		hours = 24
+	}
+
+	claims := Claims{
+		UserID:   userID,
+		Email:    email,
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(hours) * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func ParseToken(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+
+	return claims, nil
+}
