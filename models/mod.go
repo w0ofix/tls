5bda3a7fec8c2732f1de21a4ffeee9f340b513d5
w0ofix <dev-wolfix@proton.me>
2026-07-18 02:16:34 +0200

    fix: rename package lol and set status to int and default value to 0

--- models/mod.go
+++ models/mod.go
@@ -1,14 +1,13 @@
-package mod
+package models
 
 type Mod struct {
-	ID          string `gorm:"type:char(36);primaryKey" json:"id"`
 	Name        string `gorm:"unique;not null" json:"name"`
 	Description string `json:"description"`
 	Author      string `gorm:"type:char(36);primaryKey" json:"author"`
-	Game 	  	string `json:"game"`
+	Game        string `json:"game"`
 	Category    string `json:"category"`
 	Price       int    `json:"price"`
 	Image       string `json:"image"`
 	Path        string `json:"path"`
-	Status      string `json:"status"`
+	Status      int    `gorm:"default:0" json:"status"`
 }
